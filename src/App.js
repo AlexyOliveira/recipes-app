2fb6a7ea09f4d7a89c0d263c81b9258edee1a83b
alexyoliveira <cupofteaoficial@gmail.com>
2023-01-26 16:29:14 -0300

    Tela de login pronta

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Login from './pages/Login';
+import store from './redux/store';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Provider store={ store }>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          {/* <Route exact path="/game" component={ Game } />
+      <Route path="/settings" component={ Config } />
+      <Route path="/feedback" component={ Feedback } /> */}
+        </Switch>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    isRequired: false,
+  };
+
+  handleChange = (ev) => {
+    const { target } = ev;
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      const { email, password } = this.state;
+      const n = 6;
+      const isVal = email.includes('@') && email.includes('.com') && password.length > n;
+      this.setState({ isRequired: isVal });
+    });
+  };
+
+  handleSubmit = async () => {
+    const { email } = this.state;
+    // await fechtTokenApi();
+    const { history } = this.props;
+    // const { nick, email } = this.state;
+    // dispatch(addName(nick));
+    // dispatch(addEmail(email));
+    // this.gravatar();
+    // this.setState({
+    //   isRequired: false,
+    // });
+    history.push('/meals');
+    console.log(this.props);
+
+    localStorage.setItem('user', JSON.stringify({ email }));
+  };
+
+  render() {
+    const { password, email, isRequired } = this.state;
+    return (
+      <form>
+        <input
+          onChange={ this.handleChange }
+          data-testid="email-input"
+          type="email"
+          name="email"
+          value={ email }
+        />
+        <input
+          onChange={ this.handleChange }
+          data-testid="password-input"
+          type="password"
+          name="password"
+          value={ password }
+        />
+        <button
+          disabled={ !isRequired }
+          onClick={ this.handleSubmit }
+          data-testid="login-submit-btn"
+          type="button"
+        >
+          {' '}
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
