db0c45a0d7a6a58b128bc27c38527e7d2bd0e229
IzabelaFattori <izabela.fattori@hotmail.com>
2023-02-03 20:58:36 -0300

    feat: Items renderizados na pagina de detalhes - requisito 25

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -6,6 +6,8 @@ function RecipeDetails() {
   const location = useLocation();
   const [recipe, setRecipe] = useState([]);
 
+  const magicNumber = -11;
+
   useEffect(() => {
     const id = location.pathname.split('/')[2];
     const type = location.pathname.split('/')[1];
@@ -13,10 +15,10 @@ function RecipeDetails() {
     const getRecipe = async () => {
       if (type === 'meals') {
         const meal = await getMealById(id);
-        setRecipe(meal);
+        setRecipe([meal]);
       } else {
         const drink = await getDrinkById(id);
-        setRecipe(drink);
+        setRecipe([drink]);
       }
     };
 
@@ -26,7 +28,51 @@ function RecipeDetails() {
   return (
     <div>
       <h1>RecipeDetails</h1>
-      {recipe.idDrink || recipe.idMeal}
+      {recipe.map((item, index) => (
+        <div key={ index }>
+          <img
+            src={ item.strMealThumb || item.strDrinkThumb }
+            alt={ item.strMeal }
+            style={ { width: '200px' } }
+            data-testid="recipe-photo"
+          />
+          <h2 data-testid="recipe-title">{item.strMeal || item.strDrink}</h2>
+          <h3 data-testid="recipe-category">{item.strCategory}</h3>
+          <h3 data-testid="recipe-category">{item.strAlcoholic}</h3>
+          <h3>Ingredients</h3>
+          <ul>
+            {Object.keys(item).reduce((acc, key) => {
+              if (key.includes('Ingredient') && item[key] !== '' && item[key] !== null) {
+                return [...acc, item[key]];
+              }
+              return acc;
+            }, []).map((ingredient, i) => (
+              <li
+                key={ i }
+                data-testid={ `${i}-ingredient-name-and-measure` }
+              >
+                {`${ingredient} - ${item[`strMeasure${i + 1}`]}`}
+              </li>
+            ))}
+          </ul>
+          <h3>Instructions</h3>
+          <p data-testid="instructions">{item.strInstructions}</p>
+          {/* use replace in the video to work */}
+          {item.strYoutube && (
+            <>
+              <h3>Video</h3>
+              <iframe
+                data-testid="video"
+                title="recipe"
+                width="360"
+                height="315"
+                src={ `https://www.youtube.com/embed/${item.strYoutube.slice(magicNumber)}` }
+              />
+            </>
+
+          )}
+        </div>
+      ))}
     </div>
   );
 }
