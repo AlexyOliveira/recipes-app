ef025021f9e82459c4bdb6041c36ff58450bcb22
alexyoliveira <cupofteaoficial@gmail.com>
2023-02-03 21:58:31 -0300

    proptypes exportado no recipes

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { getMealsbyId } from '../services/api';
 import { getDrinksbyId } from '../services/drinksAPI';
 
