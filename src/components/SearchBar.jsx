683ab119467d578348d196e55a6afce8ec0aaee4
alexyoliveira <cupofteaoficial@gmail.com>
2023-02-14 15:18:10 -0300

    letter word correction

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -152,7 +152,7 @@ class SearchBar extends Component {
               type="radio"
               onChange={ this.handleChange }
             />
-            First lette
+            First letter
           </label>
         </div>
 
